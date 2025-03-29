b2bdb804775a1434554fb1b10b1edcc0495388f0
Anton Norman <normananton03@gmail.com>
2025-03-29 14:37:14 +0100

    added entry point

--- src/index.js
+++ src/index.js
@@ -3,28 +3,33 @@ import 'dotenv/config'
 import { Client, Events, GatewayIntentBits, MessageMentions } from 'discord.js'
 import languageMachine from './detect.js'
 
-const danishMachine = new languageMachine('da')
+const main = async () => {
+    const danishMachine = new languageMachine('da')
 
-const client = new Client({
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent
-    ]
-})
+    const client = new Client({
+        intents: [
+            GatewayIntentBits.Guilds,
+            GatewayIntentBits.GuildMessages,
+            GatewayIntentBits.MessageContent
+        ]
+    })
 
-client.once(Events.ClientReady, async (readyClient) => {
-    console.log(`Logged in as ${readyClient.user.tag}`)
-})
+    client.once(Events.ClientReady, async (readyClient) => {
+        console.log(`Logged in as ${readyClient.user.tag}`)
+    })
 
-client.on(Events.MessageCreate, async (message) => {
-    if (message.author.id === process.env.APP_ID) { return }
-    if (await danishMachine.isLanguage(message.content)) { 
-        console.log(message.content)
-        return 
-    }
-    message.reply("Kun dansk for helvede")
-})
+    client.on(Events.MessageCreate, async (message) => {
+        if (message.author.id === process.env.APP_ID) { return }
+        if (await danishMachine.isLanguage(message.content)) { 
+            console.log(message.content)
+            return 
+        }
+        message.reply("Kun dansk for helvede")
+    })
 
-// console.log(process.env.DISCORD_TOKEN)
-await client.login(process.env.DISCORD_TOKEN)
+    // console.log(process.env.DISCORD_TOKEN)
+    await client.login(process.env.DISCORD_TOKEN)
+}
+
+
+export default main
